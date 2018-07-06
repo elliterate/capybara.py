dc6b4ee11d06e48e40aaeb57097468c40d6764b2
Ian Lesperance <ian@elliterate.com>
2018-07-06 18:06:53 +0000

    Document assert_all/none_of_selectors

--- capybara/node/matchers.py
+++ capybara/node/matchers.py
@@ -159,6 +159,29 @@ class MatchersMixin(object):
         return assert_selector()
 
     def assert_all_of_selectors(self, selector, *locators, **kwargs):
+        """
+        Asserts that all of the provided selectors are present on the given page or descendants of
+        the current node. If options are provided, the assertion will check that each locator is
+        present with those options as well (other than ``wait``). ::
+
+            page.assert_all_of_selectors("custom", "Tom", "Joe", visible="all")
+            page.assert_all_of_selectors("css", "#my_dif", "a.not_clicked")
+
+        It accepts all options that :meth:`find_all` accepts, such as ``text`` and ``visible``.
+
+        The ``wait`` option applies to all of the selectors as a group, so all of the locators must
+        be present within ``wait`` (defaults to :data:`capybara.default_max_wait_time`) seconds.
+
+        If the given selector is not a valid selector, the first argument is assumed to be a locator
+        and the default selector will be used.
+
+        Args:
+            selector (str, optional): The name of the selector to use. Defaults to
+                :data:`capybara.default_selector`.
+            *locators (str): Variable length list of locators.
+            **kwargs: Arbitrary keyword arguments for :class:`SelectorQuery`.
+        """
+
         wait = kwargs['wait'] if 'wait' in kwargs else capybara.default_max_wait_time
 
         if not isinstance(selector, Hashable) or selector not in selectors:
@@ -175,6 +198,28 @@ class MatchersMixin(object):
         return assert_all_of_selectors()
 
     def assert_none_of_selectors(self, selector, *locators, **kwargs):
+        """
+        Asserts that none of the provided selectors are present on the given page or descendants of
+        the current node. If options are provided, the assertion will check that each locator is
+        present with those options as well (other than ``wait``). ::
+
+            page.assert_none_of_selectors("custom", "Tom", "Joe", visible="all")
+            page.assert_none_of_selectors("css", "#my_div", "a.not_clicked")
+
+        It accepts all options that :meth:`find_all` accepts, such as ``text`` and ``visible``.
+
+        The ``wait`` option applies to all of the selectors as a group, so none of the locators must
+        be present with ``wait`` (defaults to :data:`capybara.default_max_wait_time`) seconds.
+
+        If the given selector is not a valid selector, the first argument is assumed to be a locator
+        and the default selector will be used.
+
+        Args:
+            selector (str, optional): The name of the selector to use. Defaults to
+                :data:`capybara.default_selector`.
+            *locators (str): Variable length list of locators.
+            **kwargs: Arbitrary keyword arguments for :class:`SelectorQuery`.
+        """
         wait = kwargs['wait'] if 'wait' in kwargs else capybara.default_max_wait_time
 
         if not isinstance(selector, Hashable) or selector not in selectors:
